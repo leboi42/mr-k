62b34e76320298f836c63ebf8fd931dabdc39960
john42 <leboijohn42@gmail.com>
2025-07-08 12:33:49 +0300

    fire

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "membershiphp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error . ".<br> Please create a database and import the SQL file");
+}
+
+// Start Session
+session_start();
+?>
